7ca9b7f799fbaa9134343a6fe8d389e2ec40cb70
Arya Bhagaskara <bagaskaraarya213@gmail.com>
2025-06-07 16:24:44 +0700

    fiksasi database

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // Koneksi ke Cloud SQL MySQL instance
-const db = new Sequelize("gitaroom", "root", "PASSWORD_MYSQL_KAMU", {
+const db = new Sequelize("gitaroom", "root", "gitaroom123", {
   host: "34.134.254.216",  // IP publik instance Cloud SQL kamu
   dialect: "mysql",
   dialectOptions: {
