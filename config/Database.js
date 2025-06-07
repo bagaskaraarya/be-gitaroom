b0011008826c87864cf2bb8bb75c9c5603d2aa74
Arya Bhagaskara <bagaskaraarya213@gmail.com>
2025-06-07 16:15:26 +0700

    fix database

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
 
-// Nyambungin db ke BE
-const db = new Sequelize("gitaroom", "root", "", {
-  host: "localhost",
+// Koneksi ke Cloud SQL MySQL instance
+const db = new Sequelize("gitaroom", "root", "PASSWORD_MYSQL_KAMU", {
+  host: "34.134.254.216",  // IP publik instance Cloud SQL kamu
   dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true, // Hanya kalau SSL diaktifkan
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default db;
