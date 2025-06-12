f42fab5d3f2a39602ca281d0b26d64e647b90b61
Arya Bhagaskara <bagaskaraarya213@gmail.com>
2025-06-12 14:42:48 +0700

    routes user

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import db from "./config/Database.js";
 
 const app = express();
 app.set("view engine", "ejs");
 
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 app.use(UserRoute);
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -5,11 +5,15 @@ import {
   updateUser,
   deleteUser,
   getUserById,
+  login,
+  logout
 } from "../controllers/UserController.js";
 
 const router = express.Router();
 
 // Endpoint untuk CRUD users
+router.post("/login", login);
+router.delete("logout", logout);
 router.get("/users", getUsers);
 router.get("/users/:id", getUserById);
 router.post("/users", createUser);
